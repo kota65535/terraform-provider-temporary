f8913048c114098351c0dc1c74a87d63f84d0de3
Tomohiko Ozawa <kota65535@gmail.com>
2024-06-28 16:25:38 +0900

    add test

--- internal/provider/data_source_directory_test.go
+++ internal/provider/data_source_directory_test.go
@@ -2,6 +2,8 @@ package provider
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/require"
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -15,7 +17,34 @@ func TestAccDataSourceTemporaryDirectory(t *testing.T) {
 			{
 				Config: testAccDataSourceTemporaryDirectory(),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.temporary_directory.main", "id", ".terraform/tmp/main"),
+					resource.TestCheckResourceAttrWith("data.temporary_directory.main", "id", func(s string) error {
+						info, err := os.Stat(s)
+						require.NoError(t, err)
+						if !info.IsDir() {
+							return fmt.Errorf("'%s' should be a directory", s)
+						}
+						if info.Name() != "main" {
+							return fmt.Errorf("directory name should be 'main', got '%s'", info.Name())
+						}
+						return nil
+					}),
+				),
+			},
+			{
+				Config: testAccDataSourceTemporaryDirectoryWithBase(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrWith("data.temporary_directory.main", "id", func(s string) error {
+						info, err := os.Stat(s)
+						require.NoError(t, err)
+						if !info.IsDir() {
+							return fmt.Errorf("'%s' should be a directory", s)
+						}
+						expectedId := ".terraform/tmp/main"
+						if s != expectedId {
+							return fmt.Errorf("id should be '%s', got '%s'", expectedId, s)
+						}
+						return nil
+					}),
 				),
 			},
 		},
@@ -24,12 +53,20 @@ func TestAccDataSourceTemporaryDirectory(t *testing.T) {
 
 func testAccDataSourceTemporaryDirectory() string {
 	return fmt.Sprintf(`
+		data "temporary_directory" "main" {
+			name = "main"
+		}
+	`)
+}
+
+func testAccDataSourceTemporaryDirectoryWithBase() string {
+	return fmt.Sprintf(`
     provider "temporary" {
-		base = "${path.root}/.terraform/tmp"
+		  base = "${path.root}/.terraform/tmp"
     }
 
-	data "temporary_directory" "main" {
-		name = "main"
-	}
+		data "temporary_directory" "main" {
+			name = "main"
+		}
 	`)
 }
